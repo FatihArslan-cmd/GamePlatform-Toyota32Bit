c01ca0329ecd3ecdbb7440746b8431fb3d0ef932
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-12-21 20:08:41 +0300

    Config files added in react native for lerna

--- packages/GameCenter/metro.config.js
+++ packages/GameCenter/metro.config.js
@@ -1,11 +1,19 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  watchFolders: [
+    // Lerna root düzeyindeki node_modules
+    path.resolve(__dirname, '../../node_modules'),
+    // Shared klasörü
+    path.resolve(__dirname, '../Bingo'),
+  ],
+  resolver: {
+    extraNodeModules: {
+      react: path.resolve(__dirname, '../../node_modules/react'),
+      'react-native': path.resolve(__dirname, '../../node_modules/react-native'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
