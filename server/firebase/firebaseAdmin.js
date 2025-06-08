9658c4db5463aa6c962858e8e996b722f1a41aa8
Fatih <fatih.arslan12@ogr.sakarya.edu.tr>
2025-06-09 00:42:06 +0300

    package-lock.json removed

--- server/firebase/firebaseAdmin.js
+++ server/firebase/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./gamecenter-a93db-firebase-adminsdk-fbsvc-1dba394375.json");
+const serviceAccount = require("./gamecenter-a93db-firebase-adminsdk-fbsvc-6dbb6b2a03.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
